873e06dcaed58f17ad697ab29d320540f81b5b4f
XInfinityDarkX <35394539+XInfinityDarkX@users.noreply.github.com>
2018-03-13 21:30:59 +0300

    TESSend

---
+++ tessend.lua
@@ -0,0 +1,31 @@
+﻿local component=require("component")
+local dd=component.disk_drive
+local shell=require("shell")
+local io=require("io")
+args=shell.parse(...)
+
+if args[1]==nil then
+error("УКАЖИТЕ АРГУМЕНТЫ!1-ид дискеты,2-кому,3-от кого,4-файл")
+end
+
+path="/mnt/"..args[1].."/"
+
+file=io.open(path.."to","w")
+file:write(args[2])
+file:close()
+
+file=io.open(path.."from","w")
+file:write(args[3])
+file:close()
+
+file=io.open(path.."","r")
+text=file:read(99999)
+file:close()
+
+file=io.open(path.."msg","w")
+file:write(text)
+file:close()
+
+file=io.open(path.."header","w")
+file:write("TESNET)
+file:close()
